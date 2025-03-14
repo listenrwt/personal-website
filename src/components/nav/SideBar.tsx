e528952d2d5444c90eb16411dfe374dde4741b70
listenrwt <listenrwtprivate@gmail.com>
2025-03-14 22:10:45 +0800

    update SideBar component with improved animation and navigation links

--- src/components/nav/SideBar.tsx
+++ src/components/nav/SideBar.tsx
@@ -1,15 +1,15 @@
-"use client";
-import { useEffect, useState } from "react";
-import styles from "./sidebar.module.scss";
-import { motion } from "framer-motion";
-import { useTranslations } from "next-intl";
+'use client';
+import { useEffect, useState } from 'react';
+import styles from './sidebar.module.scss';
+import { motion } from 'framer-motion';
+import { useTranslations } from 'next-intl';
 
 export const SideBar = () => {
-  const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState('');
   // const t = useTranslations("Index");
 
   useEffect(() => {
-    const sections = document.querySelectorAll(".section-wrapper");
+    const sections = document.querySelectorAll('.section-wrapper');
 
     const options = {
       threshold: 0.3,
@@ -35,48 +35,69 @@ export const SideBar = () => {
       transition={{ duration: 0.5 }}
       className={styles.sideBar}
     >
-      <span className={styles.logo}>
-        B<span>.</span>
-      </span>
+      <motion.b
+        initial={{ x: -70 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.5, delay: 0.1 }}
+        onClick={() => {
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+          setSelected('hero');
+        }}
+        className={styles.logo}
+      >
+        W<span>L</span>
+      </motion.b>
       <motion.a
         initial={{ x: -70 }}
         animate={{ x: 0 }}
         transition={{ duration: 0.5, delay: 0.1 }}
-        href="#about"
+        href='#about'
         onClick={() => {
-          setSelected("about");
+          setSelected('about');
         }}
-        className={selected === "about" ? styles.selected : ""}
+        className={selected === 'about' ? styles.selected : ''}
       >
         About
       </motion.a>
       <motion.a
         initial={{ x: -70 }}
         animate={{ x: 0 }}
-        transition={{ duration: 0.5, delay: 0.2 }}
-        href="#projects"
-        onClick={() => setSelected("projects")}
-        className={selected === "projects" ? styles.selected : ""}
+        transition={{ duration: 0.5, delay: 0.1 }}
+        href='#education'
+        onClick={() => {
+          setSelected('education');
+        }}
+        className={selected === 'education' ? styles.selected : ''}
       >
-        Projects
+        Education
       </motion.a>
       <motion.a
         initial={{ x: -70 }}
         animate={{ x: 0 }}
         transition={{ duration: 0.5, delay: 0.3 }}
-        href="#experience"
-        onClick={() => setSelected("experience")}
-        className={selected === "experience" ? styles.selected : ""}
+        href='#experience'
+        onClick={() => setSelected('experience')}
+        className={selected === 'experience' ? styles.selected : ''}
       >
         Exp.
       </motion.a>
       <motion.a
         initial={{ x: -70 }}
         animate={{ x: 0 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        href='#projects'
+        onClick={() => setSelected('projects')}
+        className={selected === 'projects' ? styles.selected : ''}
+      >
+        Projects
+      </motion.a>
+      <motion.a
+        initial={{ x: -70 }}
+        animate={{ x: 0 }}
         transition={{ duration: 0.5, delay: 0.4 }}
-        href="#contact"
-        onClick={() => setSelected("contact")}
-        className={selected === "contact" ? styles.selected : ""}
+        href='#contact'
+        onClick={() => setSelected('contact')}
+        className={selected === 'contact' ? styles.selected : ''}
       >
         Contact
       </motion.a>
