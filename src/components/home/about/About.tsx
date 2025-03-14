349a5fe668ed96e81eba80e2b5c0c2939a26d30f
listenrwt <listenrwtprivate@gmail.com>
2025-03-14 22:08:24 +0800

    update About component with new personal introduction and background

--- src/components/home/about/About.tsx
+++ src/components/home/about/About.tsx
@@ -1,31 +1,31 @@
-"use client";
-import { MyLinks } from "@/components/nav/MyLinks";
-import { Reveal } from "@/components/common/Reveal";
-import { SectionHeader } from "@/components/common/SectionHeader";
-import styles from "./about.module.scss";
-import { Stats } from "./Stats";
-import { AiOutlineArrowRight } from "react-icons/ai";
+'use client';
+import { MyLinks } from '@/components/nav/MyLinks';
+import { Reveal } from '@/components/common/Reveal';
+import { SectionHeader } from '@/components/common/SectionHeader';
+import styles from './about.module.scss';
+import { Stats } from './Stats';
+import { AiOutlineArrowRight } from 'react-icons/ai';
 
 export const About = () => {
   return (
-    <section id="about" className="section-wrapper">
-      <SectionHeader title="About" dir="l" />
+    <section id='about' className='section-wrapper'>
+      <SectionHeader title='About' dir='l' />
       <div className={styles.about}>
         <div>
           <Reveal>
             <p className={`${styles.aboutText} ${styles.highlightFirstLetter}`}>
-              Hey! I&apos;m Bob, if you haven&apos;t already gathered that by
-              now. I&apos;m a painter turned software engineer from Daytona,
-              Florida. I specialize in the backend, primarily Node and Rust, but
-              love building with whatever tools are right for the job.
+              Hi, I am Winter Lau. I am majoring in BSc in Computer Science
+              (CSCIN) with a minor in Data Analytics and Informatics from The
+              Chinese University of Hong Kong (CUHK).
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.aboutText}>
-              A foolish person who starve for new stuff - technology ,news ,3D
-              modeling ,boxing , basketball and bad jokes 2 a person who is
-              graduated from HKUST Computer Science.He love learning in depth
-              for topics and seeking more creative projects.
+              As a software developer with a passion for innovation, I am
+              excited to bring my skills and expertise to a dynamic team. With a
+              strong foundation in computer science and data analytics, I thrive
+              in environments that require creativity, problem-solving, and
+              collaboration.
             </p>
           </Reveal>
 
