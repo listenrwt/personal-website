363613da9d772e0ab2611dc22fe415f75f7ce137
listenrwt <listenrwtprivate@gmail.com>
2025-03-14 22:13:50 +0800

    refactor Project component for consistent styling and improved readability

--- src/components/home/projects/Project.tsx
+++ src/components/home/projects/Project.tsx
@@ -1,10 +1,10 @@
-import { Reveal } from "@/components/common/Reveal";
-import { useAnimation, useInView, motion } from "framer-motion";
-import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
-import { AiFillGithub, AiOutlineExport } from "react-icons/ai";
-import { ProjectModal } from "./ProjectModal";
-import styles from "./projects.module.scss";
+import { Reveal } from '@/components/common/Reveal';
+import { useAnimation, useInView, motion } from 'framer-motion';
+import Link from 'next/link';
+import { JSX, useEffect, useRef, useState } from 'react';
+import { AiFillGithub, AiOutlineExport } from 'react-icons/ai';
+import { ProjectModal } from './ProjectModal';
+import styles from './projects.module.scss';
 
 interface Props {
   modalContent: JSX.Element;
@@ -36,9 +36,9 @@ export const Project = ({
 
   useEffect(() => {
     if (isInView) {
-      controls.start("visible");
+      controls.start('visible');
     } else {
-      controls.start("hidden");
+      controls.start('hidden');
     }
   }, [isInView, controls]);
 
@@ -50,7 +50,7 @@ export const Project = ({
           hidden: { opacity: 0, y: 100 },
           visible: { opacity: 1, y: 0 },
         }}
-        initial="hidden"
+        initial='hidden'
         animate={controls}
         transition={{ duration: 0.75 }}
       >
@@ -64,33 +64,33 @@ export const Project = ({
             src={imgSrc}
             alt={`An image of the ${title} project.`}
             style={{
-              width: hovered ? "90%" : "85%",
-              rotate: hovered ? "2deg" : "0deg",
+              width: hovered ? '90%' : '85%',
+              rotate: hovered ? '2deg' : '0deg',
             }}
           />
         </div>
         <div className={styles.projectCopy}>
-          <Reveal width="100%">
+          <Reveal width='100%'>
             <div className={styles.projectTitle}>
               <h4>{title}</h4>
               <div className={styles.projectTitleLine} />
 
-              <Link href={code} target="_blank" rel="nofollow">
-                <AiFillGithub size="2.8rem" />
+              <Link href={code} target='_blank' rel='nofollow'>
+                <AiFillGithub size='2.8rem' />
               </Link>
 
-              <Link href={projectLink} target="_blank" rel="nofollow">
-                <AiOutlineExport size="2.8rem" />
+              <Link href={projectLink} target='_blank' rel='nofollow'>
+                <AiOutlineExport size='2.8rem' />
               </Link>
             </div>
           </Reveal>
           <Reveal>
-            <div className={styles.projectTech}>{tech.join(" - ")}</div>
+            <div className={styles.projectTech}>{tech.join(' - ')}</div>
           </Reveal>
           <Reveal>
             <p className={styles.projectDescription}>
-              {description}{" "}
-              <span onClick={() => setIsOpen(true)}>Learn more {">"}</span>
+              {description}{' '}
+              <span onClick={() => setIsOpen(true)}>Learn more {'>'}</span>
             </p>
           </Reveal>
         </div>
