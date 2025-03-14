19df04812e137056c9a3a13d2536304ef09cd4f0
listenrwt <listenrwtprivate@gmail.com>
2025-03-14 22:08:40 +0800

    add Education component with EducationItem and styles

---
+++ src/components/home/education/Education.tsx
@@ -0,0 +1,40 @@
+'use client';
+import { SectionHeader } from '@/components/common/SectionHeader';
+import { EdcuationItem } from './EducationItem';
+
+export const Education = () => {
+  return (
+    <section className='section-wrapper' id='education'>
+      <SectionHeader title='Education' dir='r' />
+      {education.map((item) => (
+        <EdcuationItem key={item.title} {...item} />
+      ))}
+    </section>
+  );
+};
+
+const education = [
+  {
+    title: 'The Chinese University of Hong Kong (CUHK)',
+    major: 'BSc in Computer Science (CSCIN)',
+    minor: ', minor in Data Analytics and Informatics',
+    time: 'Sep 2022 - Jun 2026 (expected)',
+    description: '',
+    courses: [
+      'Software Engineering',
+      'Data Structures',
+      'Database Systems',
+      'Machine Learning',
+      'Algorithms',
+      'Operating Systems',
+    ],
+  },
+  {
+    title: 'Po Leung Kuk No.1 W.H. Cheung College',
+    major: '',
+    minor: '',
+    time: '2015 - 2022',
+    description: '',
+    courses: ['Physics', 'Chemistry', 'ICT', 'M2'],
+  },
+];
---
+++ src/components/home/education/EducationItem.tsx
@@ -0,0 +1,52 @@
+import { Reveal } from '@/components/common/Reveal';
+import styles from './education.module.scss';
+
+interface Props {
+  title: string;
+  major: string;
+  minor: string;
+  time: string;
+  description: string;
+  courses: string[];
+}
+
+export const EdcuationItem = ({
+  title,
+  major,
+  minor,
+  time,
+  description,
+  courses,
+}: Props) => {
+  return (
+    <div className={styles.education}>
+      <div className={styles.heading}>
+        <Reveal>
+          <span className={styles.title}>{title}</span>
+        </Reveal>
+        <Reveal>
+          <span>{time}</span>
+        </Reveal>
+      </div>
+
+      <div className={styles.heading}>
+        <Reveal>
+          <span className={styles.degree}>{major}</span>
+          <span>{minor}</span>
+        </Reveal>
+      </div>
+      <Reveal>
+        <p className={styles.description}>{description}</p>
+      </Reveal>
+      <Reveal>
+        <div className={styles.tech}>
+          {courses.map((item) => (
+            <span key={item} className='chip'>
+              {item}
+            </span>
+          ))}
+        </div>
+      </Reveal>
+    </div>
+  );
+};
