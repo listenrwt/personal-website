f962e61c9bc54fecc11974162bdddb909acafab7
listenrwt <listenrwtprivate@gmail.com>
2025-03-15 18:29:47 +0800

    style: update Heading component to use consistent quote style and comment out resume button

--- src/components/nav/Heading.tsx
+++ src/components/nav/Heading.tsx
@@ -1,17 +1,13 @@
-"use client";
-import styles from "./heading.module.scss";
-import { MyLinks } from "./MyLinks";
-import { OutlineButton } from "@/components/common/buttons/OutlineButton";
+'use client';
+import styles from './heading.module.scss';
+import { MyLinks } from './MyLinks';
+import { OutlineButton } from '@/components/common/buttons/OutlineButton';
 
 export const Heading = () => {
   return (
     <header className={styles.heading}>
       <MyLinks />
-      <OutlineButton
-        onClick={() => window.open("/LAM_ALEXANDER_CV_plan_text.pdf")}
-      >
-        My resume
-      </OutlineButton>
+      {/* <OutlineButton onClick={() => window.open('')}>My resume</OutlineButton> */}
     </header>
   );
 };
